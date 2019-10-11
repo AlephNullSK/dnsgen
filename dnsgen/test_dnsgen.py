b0ee88f023cdd951b264c255eea3e1c3f6702ac2
srilakshmishivakumar <srilakshmi.shk@gmail.com>
2019-10-10 22:07:37 -0500

    Modified the test cases for partiate domain method

--- dnsgen/test_dnsgen.py
+++ dnsgen/test_dnsgen.py
@@ -1,32 +1,6 @@
 
 import unittest
-import tldextract
-
 import dnsgen
-
-
-# domain = "....."
-# domain = ".?@#3.fde!.@.qweqw"
-# domain = "1.?@#3.fde!.@.qweqw"
-# domain = "www.letter.com.cn"
-
-# def partiate_domain(domain):
-# 	'''
-# 	Split domain base on subdomain levels.
-# 	TLD is taken as one part, regardless of its levels (.co.uk, .com, ...)
-# 	'''
-
-# 	# test.1.foo.example.com -> [test, 1, foo, example, com]
-# 	# test.2.foo.example.com.cn -> [test, 2, foo, example, com.cn]
-# 	# test.example.co.uk -> [test, example, co.uk]
-
-# 	ext = tldextract.extract(domain.lower())
-# 	parts = (ext.subdomain.split('.') + [ext.domain, ext.suffix])
-
-# 	return [p for p in parts if p]
-	
-# print(partiate_domain(domain))
-
 
 class Test_PartiateDomain(unittest.TestCase):
     def test_generalDomains(self):
